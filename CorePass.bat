5aeefdf3fd8e43f8ab438f55ff6b5f4aac8b7e09
flavdotexe <flavxfer@pm.me>
2025-08-14 18:54:18 -0300

    CorePass.bat

--- CorePass.bat
+++ CorePass.bat
@@ -1,8 +1,5 @@
 @echo off
-REM ========================================
-REM CorePass Run Script (Windows)
-REM Edite o caminho do projeto abaixo
-REM ========================================
+REM =======================================================================================
 
 SET PROJECT_PATH=C:\Users\your\path\data\CorePass-MVP-CLI\CorePass.CLI\CorePass.CLI.csproj
 
@@ -10,3 +7,5 @@ echo Running CorePass CLI...
 dotnet run -c Release --project "%PROJECT_PATH%"
 
 pause
+
+REM =======================================================================================
